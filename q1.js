46e46a8ce215f8008c0810b423d20cc1109723c2
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-09-15 23:52:49 +0530

    application form using prompt user input in js via console ,ethod

---
+++ q1.js
@@ -0,0 +1,15 @@
+// //A university is developing an online admission form. The form
+// requires storing:
+// - The applicant’s full name.
+// - Their current age.
+// - The mathematical constant PI (3.14159) for internal calculations.
+// Declare three variables using let, var, and const to store this
+// information and display them.
+let username = prompt("Enter your name");
+var age = prompt("Enter your age:");
+const PI = 3.14;
+console.log("Applicant's full  name is :"+ username);
+console.log("Current  age is :"+ age);
+console.log("Value of PI is "+ PI);
+
+
