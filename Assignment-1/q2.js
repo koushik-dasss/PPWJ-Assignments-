af6ba3874e6625c3b720254dc31f34fd8d117ca1
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-09-16 23:46:20 +0530

    Message display as desired in js

---
+++ Assignment-1/q2.js
@@ -0,0 +1,6 @@
+// In a chat app, a user types the sentence:
+// He said, ”I’m learning JavaScript!”
+// You need to correctly store this in a variable and display it.
+
+let message = "\"I'm Learning JavaScript!\"";
+console.log(message);
