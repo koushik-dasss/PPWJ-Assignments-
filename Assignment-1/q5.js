9daaefe03950dffe47c735dd7a0168cfee03ecbe
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-09-18 23:15:37 +0530

    outp[ut qs

---
+++ Assignment-1/q5.js
@@ -0,0 +1,13 @@
+// When processing user posts, the following calculations are made.
+// Predict the outputs:
+// - 1 + ”1”
+// - 1 - ”1”
+// - ”10” / ”2”
+// - ”hello” * 2
+console.log(1+"1"); // 11 
+console.log(1-"1"); // 0 
+console.log("10"/"2"); // 5 
+console.log("hello"*2); // NaN 
+
+
+
