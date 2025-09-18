eb63e313975d2c887e616523fb3c6e266240f7e8
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-09-18 22:52:29 +0530

    type of all diff data types , with some exceptions

---
+++ Assignment-1/q3.js
@@ -0,0 +1,28 @@
+// A website stores user profile data. Create variables for:
+// - Username ”Hello”
+// - Login PIN 1234
+// - Big user ID 12345678901234567890n
+// - Premium membership status true
+// - A unique symbol ID
+// - Undefined profile picture
+// - Empty status (null)
+// Print the type of each value.
+let username = "Hello";
+const PIN = 1234;
+let userID = 123456789123456789n;
+let premimum_membership = true;
+const ID = Symbol(1234578237846);
+let prfp = undefined ; 
+let e_status = null ; 
+console.log(typeof(username));
+console.log(typeof(PIN));
+console.log(typeof(userID));
+console.log(typeof(premimum_membership));
+console.log(typeof(ID));
+console.log(typeof(prfp));
+console.log(typeof(e_status));
+
+
+
+
+
