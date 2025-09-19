1accb8510e93e9372ab5896b362843cfe1547e8a
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-09-20 00:00:04 +0530

    modification

--- Assignment-1/q1.js
+++ Assignment-1/q1.js
@@ -7,7 +7,7 @@
 // information and display them.
 let username = prompt("Enter your name");
 var age = prompt("Enter your age:");
-const PI = 3.14;
+const PI = 3.14159;
 console.log("Applicant's full  name is :"+ username);
 console.log("Current  age is :"+ age);
 console.log("Value of PI is "+ PI);
---
+++ Assignment-2/q1.js
@@ -0,0 +1,7 @@
+// A school is building a web-based calculator for its students. The
+// calculator should:
+// -Take two numbers as input.
+// -Perform addition, subtraction, multiplication, division, and mod-
+// ulus.
+// -Display the results clearly to the user. Write a JavaScript program
+// to implement this calculator.
