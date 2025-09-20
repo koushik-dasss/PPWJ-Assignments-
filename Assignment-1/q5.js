6c9d6a39ae9ae94152f198db2e586744fe9e4188
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-09-20 23:49:12 +0530

    explanation and comments added

--- Assignment-1/q5.js
+++ Assignment-1/q5.js
@@ -4,10 +4,12 @@
 // - 1 - ”1”
 // - ”10” / ”2”
 // - ”hello” * 2
-console.log(1+"1"); // 11 
-console.log(1-"1"); // 0 
-console.log("10"/"2"); // 5 
-console.log("hello"*2); // NaN 
+console.log(1+"1"); // 11 , type of result is string 
+console.log(1-"1"); // 0  , type of result is number 
+console.log("10"/"2"); // 5 , type of result is number 
+console.log("hello"*2); // NaN , type of the  result is number 
+
+
 
 
 
