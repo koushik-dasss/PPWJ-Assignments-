2fb196c6d5f1aa97698cb53790bd9ca0af19c4ae
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-09-21 22:45:12 +0530

    mod added

--- Assignment-2/q1.js
+++ Assignment-2/q1.js
@@ -7,11 +7,13 @@
 // to implement this calculator.
 let num1 = Number(prompt("Enter the first  number: "));
 let num2 = Number(prompt("Enter the second  number: "));
-let sum = num1+num2 , diff = num1-num2 , mul = num1*num2 , div = num1/num2;
+let sum = num1+num2 , diff = num1-num2 , mul = num1*num2 , div = num1/num2 , mod = num1%num2;
 console.log(sum);
 console.log(diff);
 console.log(mul);
 console.log(div);
+console.log(mod);
+
 
 
 
