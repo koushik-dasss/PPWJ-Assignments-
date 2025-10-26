b3aef458c9c368ee9c1b2faabf56095bc960a3f3
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-10-26 13:21:05 +0530

    console changes

--- Assignment-3/q1.js
+++ Assignment-3/q1.js
@@ -6,9 +6,9 @@
 // - Display the updated list of item codes after performing both op-
 // erations
 let items = [101,102,103,104]; // item codes stored in an array
-console.log("Item codes are: " + items); // initial array 
+console.log("Item codes are: " , items); // initial array 
 items.push(105); // new item added at the end of the array 
 items.shift(); // first element of the array is removed 
-console.log("Updated array of item codes are :"+ items); // updated array 
+console.log("Updated array of item codes are :" ,  items); // updated array 
 
 
