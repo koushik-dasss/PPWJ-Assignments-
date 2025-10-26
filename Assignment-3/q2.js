1e146af36005ecf64ae339ee71885500b6b4d895
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-10-26 13:24:29 +0530

    q2 added , array sorting and reversing done

---
+++ Assignment-3/q2.js
@@ -0,0 +1,14 @@
+// A fruit vendor arranges the fruits on his stall in alphabetical order
+// so that customers can find them easily. However, sometimes he
+// prefers to display them in reverse order (for decoration purposes).
+// You are given an array of fruit names. Your task is to:
+// - Sort the fruits in alphabetical order.
+// - Reverse the order and display the result again.
+// - Print both arrangements to the console.
+let fruits = ["cherries" , "bananas" , "apples" , "papayas" , "mangoes"] ; // fruits in random order 
+console.log("Original array is : " , fruits);
+console.log("Sorted array is :" , fruits.sort()); // sorted array 
+console.log("Reverse array is : " , fruits.reverse()); // reversed array 
+
+
+
