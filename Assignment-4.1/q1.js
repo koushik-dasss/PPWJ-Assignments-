900e82898b85a6898ee27156ff959c94ce3080e8
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-11-11 00:25:14 +0530

    Vote or not using js if else >=

---
+++ Assignment-4.1/q1.js
@@ -0,0 +1,13 @@
+// A government portal is being developed to check whether a citizen
+// is eligible to vote in elections. The system should take the person’s
+// age as input. If the age is 18 or above, the system should display
+// a message: ”You are eligible to vote.” Otherwise, it should display:
+// ”You are not eligible to vote yet.”
+// Write a JavaScript program to implement this eligibility check using
+// comparison operators and an if-else statement.
+let age = prompt("Enter the age of the person ");// user input of age 
+if(age>=18){
+    console.log("You are eligible to vote.");
+}else{
+    console.log("You are not eligible to vote.");
+}
