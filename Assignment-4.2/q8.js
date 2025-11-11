5423d796894429cabed6ddf22c4921afc46b1f4b
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-11-11 21:25:52 +0530

    2D matrix multiplication table

---
+++ Assignment-4.2/q8.js
@@ -0,0 +1,25 @@
+// Q8 Math multiplication table In this project, you will create a math
+// multiplication table using loops. You can do this using your own
+// creativity or by following some of the following suggested steps:
+// 1. Set up a blank array to contain the final multiplication table.
+// 2. Set a value variable to specify how many values you want to
+// multiply with one another and show the results for.
+// 3. Create an outer for loop to iterate through each row and a
+// temp array to store the row values. Each row will be an array
+// of cells that will be nested into the final table.
+// 4. Add an inner for loop for the column values, which will push
+// the multiplied row and column values to the temp array.
+// 5. Add the temporary row data that contains the calculated so-
+// lutions to the main array of the final table. The final result
+// will add a row of values for the calculations.
+let multiplicationTable =[];
+let size = 10;
+for(let i = 1 ; i<=size ; i++){
+    let tempRow =[];
+    for(let j = 1 ; j<= size ; j++){
+        tempRow.push(i*j);
+    }
+    multiplicationTable.push(tempRow);
+}
+console.log("The desired multuiplication table is :" , multiplicationTable);
+
