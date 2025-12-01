1684fe325c4192e40800171b7a5346817b2dc1f1
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-12-01 21:18:25 +0530

    2d array and middle element access

---
+++ Assignment-3/q5.js
@@ -0,0 +1,11 @@
+// A simple Tic-Tac-Toe game board can be represented using a 3x3
+// two-dimensional array in JavaScript. The board contains numbers
+// 1 to 9 in sequence. Your task is to:
+// - Create a two-dimensional array to represent the board.
+// - Write code to access and display the middle cell of the board (i.e.,
+// the center element).
+let board = [[1,2,3],[4,5,6],[7,8,9]]; // tic-tac-toe board
+console.log("The board is :" ,  board); // 2d array
+let centerElement = board[1][1]; // center element 
+console.log("The center element of the board is : ", centerElement);
+
