b444aa06e5508ee57af9fddf9679c61c3b635a7e
koushik-dasss <koushikdasmagnate2005@gmail.com>
2026-01-02 16:03:18 +0530

    Use of Math module's random method and floor method to find a radnom value between two ranges

---
+++ Assignment-7/q8.js
@@ -0,0 +1,7 @@
+// Q8 A banking app needs a 6-digit OTP.
+// Task:
+// •Use Math.random() with Math.floor() to generate a random
+// 6-digit number.
+let OTP = Math.floor(100000+Math.random()*900000); // using the range of values method for generating random number
+console.log(OTP);
+
