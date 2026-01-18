6ac8c00ea7edbfcf345013cc3a7ce754ca1cabdf
koushik-dasss <koushikdasmagnate2005@gmail.com>
2026-01-18 16:02:23 +0530

    3

---
+++ Practice Folder/q3_1.js
@@ -0,0 +1,15 @@
+let username = "Koushik Das";
+console.log(typeof username );
+console.log(typeof 21);
+console.log(typeof 21.89);
+console.log(typeof 1245635651n);
+console.log(typeof Symbol("Koushik Das"));
+console.log(null);
+console.log(undefined);
+console.log(typeof false );
+
+
+
+
+
+
