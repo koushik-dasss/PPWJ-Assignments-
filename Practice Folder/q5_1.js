141ea68c6d61ec02639bb8612ca69a163051582b
koushik-dasss <koushikdasmagnate2005@gmail.com>
2026-01-18 16:18:44 +0530

    5

---
+++ Practice Folder/q5_1.js
@@ -0,0 +1,21 @@
+console.log(1+"1"); // 11
+console.log("1"+1); // 11
+console.log("1"+"1"); // 11
+console.log("1"-"1"); // 0
+console.log(1-"1"); // 0
+console.log("1"-1); // 0 
+console.log("10"/"2"); // 5 
+console.log("10"/2); // 5 
+console.log(10/"2"); // 5
+console.log("hello"*2); // string*number = NaN(not a number)
+
+// IMPLICIT CONVERSION OF NUMBER TO STRING IS ONLY THERE IN THE CASE OF "+" operator . 
+// string + string = string 
+
+
+
+
+
+
+
+
