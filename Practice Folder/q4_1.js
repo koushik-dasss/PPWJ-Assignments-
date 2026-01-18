ac7e2b27ccbafb7d780559449c82e0d1db48a513
koushik-dasss <koushikdasmagnate2005@gmail.com>
2026-01-18 16:07:28 +0530

    4

---
+++ Practice Folder/q4_1.js
@@ -0,0 +1,17 @@
+let num = 21;
+console.log(String(num)); // capital starting for each data type while explicit conversion 
+console.log(typeof String(num)); // string 
+let str = "123";
+console.log(Number(123)); // number
+console.log(typeof Number(str));
+let str2 = "";
+console.log(Boolean(str2));
+console.log(typeof Boolean(str2)); // boolean 
+
+// output of typeof (data types) will be always in small letters , starting to end 
+// while expl conv the first letter is capital . 
+
+
+
+
+
